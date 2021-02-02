34dc73c614216ef5e690b4c4af5697ce49a5c964
marth154 <37938187+marth154@users.noreply.github.com>
2021-02-02 09:43:17 +0100

    Correction des problemes

--- controllers/puzzleController.js
+++ controllers/puzzleController.js
@@ -6,7 +6,7 @@ const names = ["Mathias","Alexandre","Louis","Martin","Mathéo","Elouan"]
 function giveMePuzzle(req, res){
     
     if (!req.session.puzzleLevel){
-        req.session.puzzleLevel = 0
+        req.session.puzzleLevel = 6
     }
 
     if (req.session.puzzleLevel == 5 && !req.session.startTeleportAt){
@@ -64,4 +64,21 @@ function answer(req, res){
     }
 }
 
-module.exports = { giveMePuzzle, clue, answer }
+function checkPostion(req, res) {
+    let {x, y} = req.body
+    if(x == 36.295684 && y ==-76.224770) {
+        res.json({message: "Bien joué, le code est 'fromnsatorussia'"})
+    }
+}
+
+function sendToServer(req, res) {
+    let message = {
+        "destinataire" : "xX_d@rkg@m3ur_Xx",
+        "message" : "Hi, you find the decryption key here! check a decryptor online to see what the password is",
+        "key" : "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAIK97vFdIgdT+wtcR0q9rSwtqNLaQ9NoNMdEmlMnxIc0v4QW5b7YVAukYa8QTq34jKadhP94L3XetAtie5yjiH6/iYdLOiHAriVN4YTBI5ypfvuNESDsZVv3Baj0XU2ybCko0pEarqu4B4D2OPCaNUuGHlRRK7b+O20DAg1tqeCjAgMBAAECgYEAgLqiWeRpbO2zcSwaxeycZbNPVsyxm6Yu0UUq7lVpm/bD2zrwW0iE+DGSPNzBm2nukt7l5ORhVp1AiTUeoMwjx7J/3XKOmbIqk3jG3FOHhbEns2H4ma0mQ25gIwpAIvRBfNOgf9J+ynj95fG3erH2BJA+jYmTTszKg1zxDE6JvoECQQDxW/IlmQCjnblGKfdSCqM1FtHcA4DGkrH1CNLUY/kP3jHdiCuc655rJHLOjLVuEJ58pLdC1/Y5OBPoI5ByKyvPAkEAiqw308K5IuMT2SXapLhDbwWQ7eElXJpr+RY4qZP4Uq8IaZRVKnyKstw0PgbTemggGECvHU3hROGDu0GZ/iMO7QJBANxdEoz5UPgRvWMuVbz2MWT7XMf/+sdSkGAkmjmIvZGbUPPuq8Mt7Qnt0VR8a/n9+nXa/edtvkUnyiUcs3lJqUkCQGqKO+4O6cxa6BHqWlw5UV3HyfxpARpvbcsWfnwSujNUVMNA+lnFC461TdHlzq55t0bK039RKWSRWnMQyK4O41UCQAOjyplW4xM+Ad6gwa39mR+ysenzVOPAaDb6pHJx86razvVHR1LtP9bVniI7glaP9QKrUJUgHXF9wBXL8ExpzJk=",
+        "encrypt" : "RSA1024"
+    }
+    res.send(message)
+}
+
+module.exports = { giveMePuzzle, clue, answer, checkPostion, sendToServer }
