dfc721fc0b629b59a29300c2693df69441833560
Kobweb <sieg.alexandre@gmail.com>
2021-02-02 12:59:54 +0100

    Correct minor bugs

--- controllers/puzzleController.js
+++ controllers/puzzleController.js
@@ -6,7 +6,7 @@ const names = ["Mathias","Alexandre","Louis","Martin","Mathéo","Elouan"]
 function giveMePuzzle(req, res){
     
     if (!req.session.puzzleLevel){
-        req.session.puzzleLevel = 0
+        req.session.puzzleLevel = 5
     }
 
     if (req.session.puzzleLevel == 5 && !req.session.startTeleportAt){
